114a6d683c3eeb85de4f2b642656389e45c66e35
Lukasz Lato <latolukasz@gmail.com>
2021-06-10 11:44:39 +0200

    fixed flusher

--- where_test.go
+++ where_test.go
@@ -13,4 +13,8 @@ func TestWhere(t *testing.T) {
 	where.Append("AND Field3 = ? AND Field4 IN ?", "c", []string{"d", "e"})
 	assert.Equal(t, "1 AND Field = ? AND Field2 IN (?,?) AND Field3 = ? AND Field4 IN (?,?)", where.String())
 	assert.Equal(t, []interface{}{2, "a", "b", "c", "d", "e"}, where.GetParameters())
+	where.SetParameter(3, "b2")
+	assert.Equal(t, []interface{}{2, "a", "b2", "c", "d", "e"}, where.GetParameters())
+	where.SetParameters("c", "d", "e")
+	assert.Equal(t, []interface{}{"c", "d", "e"}, where.GetParameters())
 }
